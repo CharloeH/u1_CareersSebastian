192e9114ea6e859606756f612607e8227d2c9c05
SebastianH11 <36234258+SebastianH11@users.noreply.github.com>
2018-02-26 21:19:48 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace u1CareersSebastian
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUniName_Click(object sender, RoutedEventArgs e)
+        {
+            lblMainTitle.Content = "University of Waterloo";
+            lblSecondaryTitle.Content = "ICS3U";
+            BitmapImage bi = new BitmapImage(new Uri("http://badmintonclub.uwaterloo.ca/UniversityOfWaterloo_logo_vert_rgb.jpg"));
+            imgCenter.Source = bi;
+            btnUniName.Visibility = Visibility.Hidden;
+            btnDefault.Visibility = Visibility.Visible;
+            MessageBox.Show("<info here>");
+        }
+
+        private void btnDefault_Click(object sender, RoutedEventArgs e)
+        {
+            lblMainTitle.Content = "Sebastian's Careers Project";
+            lblSecondaryTitle.Content = "Outlining a university computer science course";
+            BitmapImage bi = new BitmapImage(new Uri("http://hhs.tldsb.on.ca/wp-content/uploads/sites/8/2014/09/HHS-logo.jpeg"));
+            imgCenter.Source = bi;
+            btnUniName.Visibility = Visibility.Visible;
+            btnDefault.Visibility = Visibility.Hidden;
+
+        }
+    }
+}
